aeeca2709e99c64238761cefff3ccc7dfa004fc9
Grbak <eden91hazard@gmail.com>
2019-06-03 10:24:04 +0300

    Update Lab3.py

--- Lab3.py
+++ Lab3.py
@@ -5,53 +5,53 @@ import itertools
 nu = 1 #Норма обучения
 eps = 0.001 #Порог, который суммарная ошибка не должна превышать
 
-def Function(net): #Функция активации
+def sigmoid_function(net): #Функция активации
 	return (1 - np.e**(-net))/(1 + np.e**(-net))
 
-def Derivative(net): #Производная функции активации
-	return 0.5*(1 - Function(net)**2)
+def derivative(net): #Производная функции активации
+	return 0.5*(1 - sigmoid_function(net)**2)
 
-class N: #Класс нейрона
+class Neuron: #Класс нейрона
 	def __init__(self, numb_w):
 		self.w = np.array([0.5]*numb_w) #Задаем вектор весов длиной numb_w, начальные веса возьмем равными 0.5
 		self.net = 0
-		self.sygma = 0
+		self.sigma = 0
 
-	def Get_out(self, x): #Функция, высчитывающая выход нейрона
+	def get_out(self, x): #Функция, высчитывающая выход нейрона
 		self.net = np.dot(self.w, x)
-		return Function(self.net)
+		return sigmoid_function(self.net)
 
-	def Correct_weight(self, x): #Функция, корректирующая веса
-		self.w +=[ nu*Derivative(self.net)*self.sygma*xi for xi in x]
+	def correct_weight(self, x): #Функция, корректирующая веса
+		self.w +=[ nu*derivative(self.net)*self.sigma*xi for xi in x]
 		self.net = 0
-		self.sygma = 0
+		self.sigma = 0
 
-	def Get_mistake(self, sygma): #Функция, высчитывающая ошибку
-		self.sygma = Derivative(self.net)*sygma
-		return [self.sygma*wi for wi in self.w[1:]] #Функция возвращает такую величину для удобства: впоследствии мы используем это, когда будем высчитывать ошибку для нейронов скрытого слоя
+	def get_mistake(self, sigma): #Функция, высчитывающая ошибку
+		self.sigma = derivative(self.net)*sigma
+		return [self.sigma*wi for wi in self.w[1:]] #Функция возвращает такую величину для удобства: впоследствии мы используем это, когда будем высчитывать ошибку для нейронов скрытого слоя
 
 
-class NN: #Класс нейронной сети
+class Neuronal_network: #Класс нейронной сети
 	def __init__(self, x, J, t):
 		self.x = x #Вектор входных данных нашей нейронной сети
 		self.J = J #Количество нейронов скрытого слоя
 		self.t = t #Вектор целевого выхода
 		self.age = 0 #Эпоха обучения
 
-		self.hide_level = [N(len(x)) for index in range(J)] #Создаем вектор нейронов скрытого слоя
-		self.output_level = [N(J+1) for index in range(len(t))] # -//- выходного слоя
+		self.hide_level = [Neuron(len(x)) for index in range(J)] #Создаем вектор нейронов скрытого слоя
+		self.output_level = [Neuron(J+1) for index in range(len(t))] # -//- выходного слоя
 
 	def Age(self): #Одна эпоха обучения
 		print('_________________Эпоха', self.age, '__________________')
 
-		hide_out = [1] + [hide_neuron.Get_out(self.x) for hide_neuron in self.hide_level] #Cоздаем вектор выходов нейронов скрытого слоя
+		hide_out = [1] + [hide_neuron.get_out(self.x) for hide_neuron in self.hide_level] #Cоздаем вектор выходов нейронов скрытого слоя
 		#"[1]" - нейрон смещения, который потребуется, когда мы будем подавать этот вектор на выходной слой
-		output_out = [output_neuron.Get_out(hide_out) for output_neuron  in self.output_level] # -//- выходного слоя
+		output_out = [output_neuron.get_out(hide_out) for output_neuron  in self.output_level] # -//- выходного слоя
 
 
-		sygma = [self.t[index] - output_out[index] for index in range(len(self.t))] #Для каждого нейрона выходного слоя высчитываем ошибку
+		sigma = [self.t[index] - output_out[index] for index in range(len(self.t))] #Для каждого нейрона выходного слоя высчитываем ошибку
 
-		squared_error =  sum(index**2 for index in sygma)**0.5 #Суммарная среднеквадратичная ошибка
+		squared_error =  sum(index**2 for index in sigma)**0.5 #Суммарная среднеквадратичная ошибка
 
 		print("Веса нейронов скрытого слоя:")
 		for neuron in self.hide_level:
@@ -67,17 +67,17 @@ class NN: #Класс нейронной сети
 		print('Суммарная среднеквадратичная ошибка:', round(squared_error, 4))
 
 
-		output_mistake = [output_neuron.Get_mistake(sygma[i]) for i, output_neuron in enumerate(self.output_level)] #Для каждого нейрона выходного слоя вызываем метод Get_mistake, используя соответствующий элемент вектора sygma
+		output_mistake = [output_neuron.get_mistake(sigma[i]) for i, output_neuron in enumerate(self.output_level)] #Для каждого нейрона выходного слоя вызываем метод get_mistake, используя соответствующий элемент вектора sigma
 		output_mistake = [sum(composition[index] for composition in output_mistake) for index in range(self.J)] #Складываем все элементы вектора output_mistake. Данная сумма необходима для вычисления ошибки на нейронах скрытого слоя
 
 		for hide_neuron, j in itertools.product(self.hide_level, range(len(output_mistake))): #Для каждого нейрона скрытого слоя вычисляем ошибку
-			hide_neuron.Get_mistake(output_mistake[j])
+			hide_neuron.get_mistake(output_mistake[j])
 
 		for hide_neuron in self.hide_level:
-			hide_neuron.Correct_weight(self.x)
+			hide_neuron.correct_weight(self.x)
 
 		for output_neuron in self.output_level:
-			output_neuron.Correct_weight(hide_out)
+			output_neuron.correct_weight(hide_out)
 
 		self.age += 1
 
@@ -87,5 +87,5 @@ class NN: #Класс нейронной сети
  		while self.Age() > eps:
  			pass
 
-n1 = NN([1, 2, 2], 1 , [0.3, 0.1])
+n1 = Neuronal_network([1, 2, 2], 1 , [0.3, 0.1])
 n1.Learning()
